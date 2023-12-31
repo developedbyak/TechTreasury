94261e177996d44cfe79a8a83c25d123438c30c9
/Akash <akashdebnathwd@gmail.com>
2023-12-31 23:10:45 +0530

    Added assets, logo of the website and navbar v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -16,6 +17,7 @@ export default function RootLayout({
         <html lang="en" className="h-full">
             <body className={cn("relative h-full antialiased")}>
                 <main className=" relative flex flex-col min-h-screen">
+                    <Navbar />
                     <div className=" flex-grow flex-1">{children}</div>
                 </main>
             </body>
