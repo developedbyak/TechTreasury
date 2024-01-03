c2645709cdded607b335b7625ae9c32b1e09c367
/Akash <akashdebnathwd@gmail.com>
2024-01-03 18:09:09 +0530

    Resolved payload cms issue not connected to mongoDB

---
+++ src/seed/index.ts
@@ -0,0 +1,12 @@
+import type { Payload } from "payload";
+
+export const seed = async (payload: Payload): Promise<void> => {
+    // create admin
+    await payload.create({
+        collection: "users",
+        data: {
+            email: "demo@payloadcms.com",
+            password: "demo",
+        },
+    });
+};
