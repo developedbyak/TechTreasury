649ac99f08181de325b88e0430342d0d38279cbf
/Akash <akashdebnathwd@gmail.com>
2024-01-13 10:32:43 +0530

    added toast from sonner lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
     title: "TechTreasury",
@@ -24,6 +25,8 @@ export default function RootLayout({
                         <div className=" flex-grow flex-1">{children}</div>
                     </Providers>
                 </main>
+
+                <Toaster position="top-center" richColors />
             </body>
         </html>
     );
